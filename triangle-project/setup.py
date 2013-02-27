c2a71cd5d65d32421df28ee7e1febd981d8c6d2f
Brandon Rhodes <brandon@rhodesmill.org>
2013-02-27 14:10:38 -0800

    Add more text to the trianglelib docstring

--- triangle-project/setup.py
+++ triangle-project/setup.py
@@ -3,7 +3,7 @@ import trianglelib
 
 setup(
     name='trianglelib',
-    description=trianglelib.__doc__,
+    description=trianglelib.__doc__.splitlines()[0],
     author='Brandon Rhodes',
     author_email='brandon@rhodesmill.org',
     py_modules=['trianglelib'],
