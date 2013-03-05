92a0e21a06ac4e8c509f03dc2132fd2bc9f7b55b
Brandon Rhodes <brandon@rhodesmill.org>
2013-03-05 13:30:35 -0500

    Changed name of handout to "Sphinx Tutorial"

--- handout/conf.py
+++ handout/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# trianglelib documentation build configuration file, created by
+# Sphinx Tutorial documentation build configuration file, created by
 # sphinx-quickstart on Wed Feb 27 14:16:34 2013.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'trianglelib'
+project = u'sphinx-tutorial'
 copyright = u'2013, Brandon Rhodes'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'trianglelibdoc'
+htmlhelp_basename = 'sphinx-tutorial-doc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -189,7 +189,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'trianglelib.tex', u'trianglelib Documentation',
+  ('index', 'sphinx-tutorial.tex', u"Brandon's Sphinx Tutorial",
    u'Brandon Rhodes', 'manual'),
 ]
 
@@ -219,7 +219,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'trianglelib', u'trianglelib Documentation',
+    ('index', 'sphinx-tutorial', u"Brandon's Sphinx Tutorial",
      [u'Brandon Rhodes'], 1)
 ]
 
@@ -233,8 +233,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'trianglelib', u'trianglelib Documentation',
-   u'Brandon Rhodes', 'trianglelib', 'One line description of project.',
+  ('index', 'sphinx-tutorial', u'Sphinx Tutorial',
+   u'Brandon Rhodes', 'sphinx-tutorial', 'One line description of project.',
    'Miscellaneous'),
 ]
 
