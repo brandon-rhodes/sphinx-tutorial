e858832d21ddad6685ee4b52036742bc6823f48a
Brandon Rhodes <brandon@rhodesmill.org>
2013-03-13 06:06:28 -0700

    Switch to real black for foreground slide color

--- show.sh
+++ show.sh
@@ -2,5 +2,5 @@
 # For a completely white background: -bg "#ffffff"
 
 exec xterm -geometry 60x22 \
- -T "Emacs" -fa "Inconsolata" -fs 20 \
- -e 'TERM=xterm-256color exec emacs -nw '"$@"
+ -T "Emacs" -fa "Inconsolata" -fs 20 -fg black \
+ -e 'exec emacs -nw slides.txt'
