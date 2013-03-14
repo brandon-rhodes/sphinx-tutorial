2be5be9386fb1fafe5015ba08bf40e680a4a1b8d
Brandon Rhodes <brandon@rhodesmill.org>
2013-03-14 08:42:19 -0700

    Up the font size for 1024×768

--- show.sh
+++ show.sh
@@ -2,5 +2,5 @@
 # For a completely white background: -bg "#ffffff"
 
 exec xterm -geometry 60x22 \
- -T "Emacs" -fa "Inconsolata" -fs 20 -fg black \
+ -T "Emacs" -fa "Inconsolata" -fs 24 -fg black \
  -e 'exec emacs -nw slides.txt'
